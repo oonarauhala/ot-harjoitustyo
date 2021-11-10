ea74533f2a946e36c3af275932ccc653ac7ab1f5
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-11-10 20:51:03 +0200

    Add ex. 2

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,9 +1,23 @@
 import unittest
 from maksukortti import Maksukortti
 
+
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(10)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_konstruktori_saldo_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_lataa_rahaa_toimii_oikein(self):
+        self.maksukortti.lataa_rahaa(10)
+        self.assertEqual(self.maksukortti.saldo, 20)
+
+    def test_maksu_kun_rahaa_on_tarpeeksi(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(3))
+
+    def test_maksu_kun_rahaa_ei_ole_tarpeeksi(self):
+        self.assertFalse(self.maksukortti.ota_rahaa(11))
