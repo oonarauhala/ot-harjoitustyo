c52492b6bfebc77a2b44096c081380c64d1a62d6
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-11-23 13:26:16 +0200

    Add coverage report

--- dokumentaatio/työaikakirjanpito.md
+++ dokumentaatio/työaikakirjanpito.md
@@ -2,3 +2,4 @@ Päivä   | Aika (h)| Tehty työ
 --- | --- | ---   
 10.11 | 1 | Alustava vaatimusmäärittelydokumentti ja sovelluksen ideointi
 21.11 | 1 | Luo yksinkertainen sovellus, jonka saa käyntiin
+22.11 | 6 | Lisää sovellukseen ikkuna, hahmo ja testi
--- tasks.py
+++ tasks.py
@@ -10,3 +10,10 @@ def start(ctx):
 def test(ctx):
     ctx.run("poetry shell")
     ctx.run("pytest src")
+
+
+@task
+def coverage_report(ctx):
+    ctx.run("poetry shell")
+    ctx.run("coverage run --branch -m pytest src")
+    ctx.run("coverage report -m")
