545f3d4c8232f4554901e1f638d9612a656ffedb
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-11-30 17:27:14 +0200

    Create coveragerc

--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -6,7 +6,5 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = App()
 
-    def test_image_load_successful(self):
-        images = ["src/media/dog1.png"]
-        self.app.load_images(images)
-        self.assertIsNot(self.app.images, [])
+    def test_app_init(self):
+        self.assertEqual(self.app.ui_sprites, [])
