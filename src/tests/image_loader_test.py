6dbc9281692e77dba189280ed646b50d06c84712
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-07 19:27:27 +0200

    Add tests for image-loader.py

---
+++ src/tests/image_loader_test.py
@@ -0,0 +1,20 @@
+import unittest
+from pygame import Surface
+from entities.image_loader import ImageLoader
+
+
+class TestImageLoader(unittest.TestCase):
+    def setUp(self):
+        self.image_loader = ImageLoader()
+
+    def test_view_1_image_load_returns_3_images(self):
+        image_list = self.image_loader.load_view_images(1)
+        self.assertEqual(len(image_list), 3)
+
+    def test_view_2_image_load_returns_3_images(self):
+        image_list = self.image_loader.load_view_images(2)
+        self.assertEqual(len(image_list), 3)
+
+    def test_load_background_returns_one_surface(self):
+        surface = self.image_loader.load_background()
+        self.assertIsInstance(surface, Surface)
