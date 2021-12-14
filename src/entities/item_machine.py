d3fbd365b2936dd791387f5202ec241ebde97ca0
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-14 21:00:39 +0200

    Add docstrings

--- src/entities/item_machine.py
+++ src/entities/item_machine.py
@@ -2,5 +2,11 @@ from random import choice
 
 
 class ItemMachine:
+    """A class that draws random items from a list."""
+
     def generate_item(self):
+        """A function that draws random items from a list.
+
+        Returns:
+            The name of a random item."""
         return choice(["food", "money"])
