98bafcc81155d048e1ebfe13b39e8b1febdc3b34
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-14 20:55:40 +0200

    Add docstrings

--- src/entities/game_sprite.py
+++ src/entities/game_sprite.py
@@ -2,7 +2,21 @@ import pygame
 
 
 class GameSprite(pygame.sprite.Sprite):
+    """A custom pygame Sprite class that creates sprites with images.
+
+    Attributes:
+        image: Image to be displayed.
+        rect: A pygame Rect object containing the sprite.
+    """
+
     def __init__(self, pos_x, pos_y, image):
+        """A class constructor for creating GameSprite objects.
+
+        Args:
+            pos_x: X coordinate in game window.
+            pos_y: Y coordinate in game window.
+            image: Image to be displayed on sprite.
+        """
         super().__init__()
         self.image = image
         self.rect = self.image.get_rect()
--- src/entities/hunger_generator.py
+++ src/entities/hunger_generator.py
@@ -2,7 +2,17 @@ from random import randint
 
 
 class HungerGenerator:
+    """A class that randomly generates hunger for a pet. A number is randomly chosen from
+    range 0-2000 and checked if it is zero.
+    """
+
     def generate_hunger(self):
+        """A function that draws a radom number from range 0-2000.
+
+        Returns:
+            True, if drawn number is zero.
+            False, if drawn number is any other than zero.
+        """
         number = randint(0, 2000)
         if number == 0:
             return True
