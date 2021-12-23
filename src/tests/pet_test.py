b12d533198f051c965696e3af7b561679d5e2fbf
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-23 14:23:41 +0200

    Fix pet tests

--- src/tests/pet_test.py
+++ src/tests/pet_test.py
@@ -6,28 +6,27 @@ from entities.user import User
 
 class TestPet(unittest.TestCase):
     def setUp(self):
-        self.pet = Pet("Pottu", "dog")
+        self.pet = Pet()
+        self.pet.set_data("Pottu", "dog", 3)
 
-    def test_pet_init(self):
-        self.assertEqual(self.pet.name, "Pottu")
-        self.assertEqual(self.pet.animal_type, "dog")
-        self.assertEqual(self.pet.hunger, 3)
-
-    def test_pet_get_animal_type(self):
+    def test_pet_get_animal_type_after_setting_data(self):
         animal_type = self.pet.get_animal_type()
         self.assertEqual(animal_type, "dog")
 
-    def test_pet_is_hungry_after_init(self):
+    def test_pet_is_hungry_after_setting_data(self):
         hunger = self.pet.is_hungry()
         self.assertEqual(hunger, True)
 
-    def test_pet_not_hugry_after_feeding_3_times(self):
-        user = Mock(wraps=User())
-        user.feed_pet(self.pet)
-        user.feed_pet(self.pet)
-        user.feed_pet(self.pet)
+    def test_pet_not_hugry_after_eating_3_times(self):
+        self.pet.eat()
+        self.pet.eat()
+        self.pet.eat()
         self.assertEqual(self.pet.is_hungry(), False)
 
     def test_pet_gets_hungrier(self):
         self.pet.get_hungrier()
         self.assertEqual(self.pet.hunger, 4)
+
+    def test_pet_export_data(self):
+        result = self.pet.export_data()
+        self.assertEqual(result, {"name": "Pottu", "type": "dog", "hunger": 3})
